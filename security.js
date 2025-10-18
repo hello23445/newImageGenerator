15c0593aa86602184d985230acfd3fbbe0422c8b
Javad <168000154+hello23445@users.noreply.github.com>
2025-10-18 23:12:51 +0400

    Add files via upload

--- security.js
+++ security.js
@@ -4,3 +4,13 @@ if (blockedUsers.includes(localStorage.getItem('user_Token'))){
     alert('Ваш аккаунт заблокирован за нарушение правил сообщества. Если вы считаете, что это ошибка, пожалуйста, свяжитесь с поддержкой.');
     window.location.href = 'block.html';
 }
+function getRandomNumber() {
+  const number = Math.floor(Math.random() * 31) + 6;
+  if (number === 6) {
+    localStorage.removeItem('subscribed');
+  }
+  return number;
+}
+
+// Вызов функции
+console.log(getRandomNumber());
