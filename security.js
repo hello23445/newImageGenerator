358c0b4c590c0bb844c16f2da8f042966a4691ca
Javad <168000154+hello23445@users.noreply.github.com>
2025-10-26 11:33:47 +0400

    Add files via upload

--- security.js
+++ security.js
@@ -1,5 +1,16 @@
 export const badWords = ['порно', 'минет', 'голая', 'тварь', 'без одежды', 'аллах', 'аллаh', 'аllah', 'сосет', 'отсасывает', 'хуй', 'член', 'обнаженная'];
-const blockedUsers = ['']
+const blockedUsers = [
+  'ChpyKGvh9QYwXp-XENoc63U1nc5jpZ',
+  'a03WfYJErB2q0AqDImFEouIySY-HYe',
+  'eaInYYq3ed0JZ4V9CbFjlZLRA91DA0',
+  'iwr6ivyBomBChh51sbxyPTtnB5CZdL',
+  'iSQgQFV81vACtznUBTjeqmEJdRbx3q',
+  'a90AJuwOOdDhq-Ja-HR5ph0fVHWl9w',
+  'ZEeqOdK3JLtnDIQJbTilX5R99xjw3O',
+  '35j6gzEK7i7FlfgkOZLgLDCwcGUuUA'
+  ]
+
+
 if (blockedUsers.includes(localStorage.getItem('user_Token'))){
     alert('Ваш аккаунт заблокирован за нарушение правил сообщества. Если вы считаете, что это ошибка, пожалуйста, свяжитесь с поддержкой.');
     window.location.href = 'block.html';
